ac2d47511c1a4b25d153906ba69548cf7ba318a7
Pär Berge <Par.Berge.ext@netent.com>
2018-08-30 09:53:10 +0200

    Fix code styling in timereport
    Rename functions so they have consistent style.
    Fix indentation.
    Other small fixes.

--- timereport/validators.py
+++ timereport/validators.py
@@ -1,10 +1,10 @@
 import re
 
 
-def validateRegex(regex, text):
+def validate_regex(regex, text):
     result = [i for i in re.finditer(regex, text) if i]
     if result:
         for match in result:
             return match.group(1)
     else:
-        return ("empty")
+        return "empty"
