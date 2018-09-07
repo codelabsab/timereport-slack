1d1538187677596ecc98618c74a42ab7eb77451c
Kamjar Gerami <kami.gerami@gmail.com>
2018-09-07 08:35:34 +0200

    timereport: add intern_arbete config parameter

--- timereport/config.py
+++ timereport/config.py
@@ -2,7 +2,7 @@
 date_regex_start = "([0-9]{4}-[0-9]{2}-[0-9]{2}|today)"
 date_regex_end = ":([0-9]{4}-[0-9]{2}-[0-9]{2})?"
 hour_regex = " ([0-9]) "
-type_regex = "(vab|betald_sjukdag|obetald_sjukdag|ledig|semester|foraldrar_ledigt)"
+type_regex = "(vab|betald_sjukdag|obetald_sjukdag|intern_arbete|semester|foraldrar_ledigt)"
 
 def validate_user_name(type_id, date_start, date_end, hours):
    user_name_regex = "(?:{typid}).(?:{start}).(?:{end})?.(?:{hours})?.?(\w+[.]\w+)".format(typid=type_id, start=date_start, end=date_end, hours=hours)
