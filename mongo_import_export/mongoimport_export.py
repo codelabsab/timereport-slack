99472caefff2d33aedfb24e41fe73bef8ff6e803
Kamjar Gerami <kami.gerami@gmail.com>
2018-10-14 08:38:15 +0200

    mongo_export: exports and imports database to new collection users_dev

---
+++ mongo_import_export/mongoimport_export.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import urllib.parse
+from pymongo import MongoClient
+from bson import BSON, decode_all
+from bson import Binary, Code
+from bson.json_util import dumps, loads
+from bson import json_util, ObjectId
+import json
+
+# MONGO DB VALUES
+MONGO_HOST = os.environ["MONGO_HOST"]
+MONGO_PORT = os.environ["MONGO_PORT"]
+MONGO_USERNAME = os.environ["MONGO_USERNAME"]
+MONGO_PASSWORD = os.environ["MONGO_PASSWORD"]
+MONGO_DBNAME = os.environ["MONGO_DBNAME"]
+MONGO_URI = "mongodb://{}:{}@{}:{}/{}".format(MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONGO_DBNAME)
+
+def lambda_handler(event, context):
+
+  mongo = MongoClient(MONGO_URI)
+  db = mongo[MONGO_DBNAME]
+
+  # export prod collection
+  with open('users.bson', 'wb+') as f:
+      for doc in db.users.find():
+          f.write(BSON.encode(doc))
+
+  # drop existing dev collection
+  db.users_dev.drop()
+  # import new from prod
+  with open('users.bson', 'rb') as f:
+      db.users_dev.insert(decode_all(f.read()))
+  mongo.close()
