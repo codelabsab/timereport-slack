ecf6f07642a4c5fc06868799e65cd929b09bfcef
Pär Berge <paer.berge@gmail.com>
2018-11-28 15:46:02 +0100

    Add unit test of lambda_handler

--- tests/test_timereport.py
+++ tests/test_timereport.py
@@ -1,8 +1,18 @@
 from timereport import api
 import json
 from . import test_data
+from mockito import when, mock, unstub
+from botocore.vendored import requests
 
 
-def test_foo():
+def test_lambda_handler():
     fake_event = json.loads(test_data.d)
-    assert api.lambda_handler(fake_event, context=None)
+    response = mock({'status_code': 200, 'text': 'Ok'})
+    when(requests).post(
+        'https://hooks.slack.com/commands/T2FG58LDV/491076166711/bVUlrKZrnElSOBUqn01FoxNf',
+        data='{"text": "vab today"}',
+        headers={'Content-Type': 'application/json'}
+    ).thenReturn(response)
+
+    assert api.lambda_handler(fake_event, context=None) == 200
+    unstub()
