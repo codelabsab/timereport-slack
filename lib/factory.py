7f7ed58f1400152f1997c100afebf4f92ae4ee8f
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2018-12-02 19:05:25 +0100

    Update factory function and daterange function

---
+++ lib/factory.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+def factory(order):
+    format_str = "%Y-%m-%d"
+    dates, events = [], []
+    user_id, user_name = order['user_id'], order['user_name']
+    cmd = order['text'].split("+")
+    action, reason, date_str = cmd[:3]
+    try:
+        hours = cmd[3]
+    except IndexError:
+        hours = "8"
+    if ":" in date_str:
+        date_str_start, date_str_stop = date_str.split(":")
+        date_obj_start = datetime.strptime(date_str_start, format_str)
+        date_obj_stop = datetime.strptime(date_str_stop, format_str)
+        for d in daterange(date_obj_start, date_obj_stop):
+            dates.append(d)
+    else:
+        date_obj = datetime.strptime(date_str, format_str)
+        dates.append(date_obj)
+    for event_date in dates:
+        e = Event(user_id, user_name, reason, event_date, hours)
+        events.append(e)
+    return events
+
+
+def daterange(start_date, stop_date):
+    delta = timedelta(days=1)
+    while start_date <= stop_date:
+        yield start_date
+        start_date += delta
