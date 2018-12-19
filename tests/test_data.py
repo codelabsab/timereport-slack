ab33df14b1ad0cdcb45084c061b766c213cc67bc
Kamjar Gerami <kami.gerami@gmail.com>
2018-12-19 13:36:53 +0100

    test_data: update text to use date_range

--- tests/test_data.py
+++ tests/test_data.py
@@ -82,5 +82,5 @@ data = {"resource": "/no-wsgi-app",
         "domainName": "hp2zas87ei.execute-api.us-east-1.amazonaws.com",
         "apiId": "hp2zas87ei"
     },
-    "body": "token=OwjK95k89vMbtbyvhvLZkXNl&team_id=T2FG58LDV&team_domain=codelabsab&channel_id=CA8THJML6&channel_name=development&user_id=U2FGC795G&user_name=kamger&command=%2Fno-wsgi&text=add+vab+2018-10-01&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT2FG58LDV%2F491076166711%2FbVUlrKZrnElSOBUqn01FoxNf&trigger_id=490225208629.83549292471.860541eab9e9c3c6d7464ea2e979c7a5",
+    "body": "token=OwjK95k89vMbtbyvhvLZkXNl&team_id=T2FG58LDV&team_domain=codelabsab&channel_id=CA8THJML6&channel_name=development&user_id=U2FGC795G&user_name=kamger&command=%2Fno-wsgi&text=add+vab+2018-10-01:2018-10-10&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT2FG58LDV%2F491076166711%2FbVUlrKZrnElSOBUqn01FoxNf&trigger_id=490225208629.83549292471.860541eab9e9c3c6d7464ea2e979c7a5",
     "isBase64Encoded": False}
