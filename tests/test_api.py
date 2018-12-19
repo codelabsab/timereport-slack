d6ff06e19cf97feaad5929686781a63150ce0e96
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2018-12-19 14:09:57 +0100

    Update test_api to work with pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,9 @@
 import timereport.api as api
 
-from tests.test_data import data
+from tests.test_data import data as event
 import os
 
-def main():
+def test_factory():
     # set os.environ SLACK_TOKEN
     os.environ["SLACK_TOKEN"] = api.config['SLACK_TOKEN']
-    api.lambda_handler(data, None)
-
-
-if __name__ == '__main__':
-    main()
-
+    api.lambda_handler(event, None)
