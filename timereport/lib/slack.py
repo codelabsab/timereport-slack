57ce2cc455680f53719040f49a76bde7c9d244ce
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2018-12-20 15:42:32 +0100

    Add documentation about the dict returned by slack_payload_extractor

--- timereport/lib/slack.py
+++ timereport/lib/slack.py
@@ -18,6 +18,18 @@ def slack_responder(url, msg):
 def slack_payload_extractor(req):
     '''
     Returns a dict containing the pure slack payload. Removes apigw metadata.
+    d =
+     "token": "secret",
+     "team_id": "team_id",
+     "team_domain": "team_domain",
+     "channel_id": "channel_id",
+     "channel_name": "channel_name",
+     "user_id": "user101",
+     "user_name": "test_user",
+     "command": "%2Fno-wsgi",
+     "text": "add+vab+2018-10-01:2018-10-10",
+     "response_url": "https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT2FG58LDV%2F4913463461%2FbVUlrKZrnElSOBUqn01FoxNf",
+     "trigger_id": "490225208629.83549292471.860541eab9e9c3c6d2234ea2e222c7a5"
     '''
     d = {}
     for param in req['body'].split("&"):
