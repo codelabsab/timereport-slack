0675b8e7269095cad3f4df6f0c4203c038f1c25c
Pär Berge <par.berge@codelabs.se>
2018-12-21 09:52:17 +0100

    Fix path to test config.json

--- tests/test_timereport.py
+++ tests/test_timereport.py
@@ -1,8 +1,9 @@
 import os
 from timereport.lib.helpers import parse_config
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def test_parsing_config():
-    test_config = parse_config()
+    test_config = parse_config(f'{dir_path}/config.json')
     test_config.get('SLACK TOKEN') == 'fake token'
 
