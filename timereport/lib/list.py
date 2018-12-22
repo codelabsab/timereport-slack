4c55154a895335d150447e4bbc99e401afd8b598
Kami Gerami <kami.gerami@gmail.com>
2018-12-22 02:43:55 +0100

    backend_api: add chalice project files to be able to deploy chalice to same backend configured already (us-east-1) It's OK that chalice python_backend_url is checked IN
    config.json: add python_backend_url for new chalice python-backend
    
    test_api.py: add two separate test_data, one for action = list, one for action: add
    
    test_data.py: two separate data - one called add, one called list
    
    add.py: create_event function - POST to new chalice python backend, add try, except to requests connection in case of wrong URL endpoint
    
    factory.py: We need to serialize the json datetime object to date.isoformat to POST to backend as application/json
    list.py: get_user_by_id function - GET from new chalice backend, add try, except to requests connection in case of wrong URL endpoint for visibility purposes while using two different URL's for backend
    
    api.py: add new python_backend_url

--- timereport/lib/list.py
+++ timereport/lib/list.py
@@ -1,5 +1,27 @@
 import botocore.vendored.requests.api as requests
 
+def get_user_by_id(url, user_id):
+    """
+    Uses new python chalice backend
+    only returns back at this point, no DB connection
+
+    :method: GET
+    :url: /timereport/user/{user_id}
+    :return: {'user_id': user_id}
+    """
+    url = url + '/' + user_id
+    res = requests.get(url=url)
+    print(f'{"="*10}')
+    print(f'python backend')
+    print(f'Response code is: {res.status_code}')
+    print(f'Url is: {res.url}')
+    print(f'{res.text}')
+    print(f'{"="*10}')
+    if res.status_code == 200:
+        return True
+    else:
+        return False
+
 
 def get_between_date(url, start_date, auth_token, end_date):
     """
@@ -14,12 +36,15 @@ def get_between_date(url, start_date, auth_token, end_date):
     :return:
     """
     params = {'access_token': auth_token, 'startDate': start_date, 'endDate': end_date}
-    res = requests.get(url=url, params=params)
-    print(f'Response code is: {res.status_code}')
-    print(f'Url is: {res.url}')
-    for resp in res.json():
-        print(f'{resp}')
-    if res.status_code == 200:
-        return True
-    else:
-        return False
+    try:
+        res = requests.get(url=url, params=params)
+        print(f'Response code is: {res.status_code}')
+        print(f'Url is: {res.url}')
+        for resp in res.json():
+            print(f'{resp}')
+        if res.status_code == 200:
+            return True
+        else:
+            return False
+    except:
+        print(f"Can't connect to {url}")
