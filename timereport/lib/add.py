7ba8ed921282f2d54413d1c5e925face88b1bf7a
Kami Gerami <kami.gerami@gmail.com>
2018-12-22 02:43:55 +0100

    backend_api: add chalice project files to be able to deploy chalice to same backend configured already (us-east-1) It's OK that chalice python_backend_url is checked IN
    config.json: add python_backend_url for new chalice python-backend
    
    test_api.py: add two separate test_data, one for action = list, one for action: add
    
    test_data.py: two separate data - one called add, one called list
    
    add.py: create_event function - POST to new chalice python backend, add try, except to requests connection in case of wrong URL endpoint
    
    factory.py: We need to serialize the json datetime object to date.isoformat to POST to backend as application/json
    list.py: get_user_by_id function - GET from new chalice backend, add try, except to requests connection in case of wrong URL endpoint for visibility purposes while using two different URL's for backend
    
    api.py: add new python_backend_url

--- timereport/lib/add.py
+++ timereport/lib/add.py
@@ -1,13 +1,37 @@
 import botocore.vendored.requests.api as requests
 
+def create_event(url, data):
+    """
+    Uses new python chalice backend
+    only returns back at this point, no DB connection
 
-def post_to_backend(url, data, auth_token):
-    params = {'access_token': auth_token}
-    res = requests.post(url=url, data=data, params=params)
+    :method: POST
+    :url: '/timereport/event'
+    :return: current_request.raw_body.decode() (same data you send in)
+    """
+    headers={'Content-Type': 'application/json'}
+    res = requests.post(url=url, data=data, headers=headers)
+    print(f'{"="*10}')
+    print(f'python backend')
     print(f'Response code is: {res.status_code}')
     print(f'Url is: {res.url}')
-    print(f'Response is: {res.text}')
+    print(f'{res.text}')
+    print(f'{"="*10}')
     if res.status_code == 200:
         return True
     else:
         return False
+
+def post_to_backend(url, data, auth_token):
+    params = {'access_token': auth_token}
+    try:
+        res = requests.post(url=url, data=data, params=params)
+        print(f'Response code is: {res.status_code}')
+        print(f'Url is: {res.url}')
+        print(f'Response is: {res.text}')
+        if res.status_code == 200:
+            return True
+        else:
+            return False
+    except:
+        print(f"Can't connect to {url}")
