e9efcfa28e2e6e8d8e0e45a75b3fa501e2dcea9c
Kamjar Gerami <kami.gerami@gmail.com>
2018-12-31 12:04:44 +0100

    chalice: move project dependencies to chalicelib dir

---
+++ timereport/chalicelib/lib/slack.py
@@ -0,0 +1,52 @@
+import os
+import botocore.vendored.requests.api as requests
+from urllib.parse import parse_qs
+
+def slack_responder(url, msg):
+    """
+    Sends post to slack_response_url
+    :param url: slack response_url
+    :param msg:
+    :return: boolean
+    """
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post(url=url, data={"text": f"{msg}"}, headers=headers)
+    if res.status_code == 200:
+        return True
+    else:
+        return False
+
+
+def slack_payload_extractor(req):
+    """
+    Extract the body data of the slack request.
+
+    :param req: The request data from slack
+    :return: dict
+    """
+
+    print(req)
+    print(type(req))
+    # parse_qs makes a list of all values, and that's why the v.pop() is necessary
+    return {k: v.pop() for (k,v) in parse_qs(req['body']).items()}
+
+
+def verify_token(token):
+    if token == os.getenv('SLACK_TOKEN', 'fake_token'):
+        return True
+    else:
+        return False
+
+
+def verify_reasons(valid_reasons, reason):
+    if reason in valid_reasons:
+        return True
+    else:
+        return False
+
+
+def verify_actions(valid_actions, action):
+    if action in valid_actions:
+        return True
+    else:
+        return False
---
+++ timereport/chalicelib/model/event.py
@@ -0,0 +1,9 @@
+def create_event(user_id, user_name, reason, event_date, hours):
+    event = {
+        'user_id': user_id,
+        'user_name': user_name,
+        'reason': reason,
+        'event_date': event_date,
+        'hours': hours,
+    }
+    return event
