02a31208d5bc275b0a8a4bc1987426c515e29c4d
parberge <paer.berge@gmail.com>
2019-02-21 19:49:34 +0100

    Remove unused function json_serial

--- timereport/chalicelib/lib/factory.py
+++ timereport/chalicelib/lib/factory.py
@@ -103,10 +103,3 @@ def date_to_string(date, format_str='%Y-%m-%d'):
     """
     return datetime.strftime(date, format_str)
 
-
-def json_serial(date):
-    """JSON serializer for objects not serializable by default json code"""
-
-    if isinstance(date, (datetime, datetime.date)):
-        return date.isoformat()
-    raise TypeError ("Type %s not serializable" % type(date))
