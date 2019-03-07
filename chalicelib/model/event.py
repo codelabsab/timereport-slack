62273c98aae7a4d3b8e5d638f0c8b2b8e0ce5bd2
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-03-07 17:15:21 +0100

    Add Event class

--- chalicelib/model/event.py
+++ chalicelib/model/event.py
@@ -1,3 +1,5 @@
+import json
+
 def create_event(user_id, user_name, reason, event_date, hours):
     format_str = "%Y-%m-%d"
     event = {
@@ -8,3 +10,17 @@ def create_event(user_id, user_name, reason, event_date, hours):
         'hours': hours,
     }
     return event
+
+class Event:
+    # Work in progress, not used.
+
+    def __init__(self, user_id, user_name, reason, event_date, hours, fmt):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.reason = reason
+        self.event_date = event_date
+        self.hours = hours
+        self.fmt = fmt
+
+    def to_json(self):
+        return json.dumps(vars(self))
