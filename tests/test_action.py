62ad41e13dc2d04b2573ad46ab5b418e82dde5e2
parberge <paer.berge@gmail.com>
2019-03-14 18:50:17 +0100

    Add test of edit action
    Refactor and remove fixture since every test of the different methods
    needs different configuration etc.
    
    Also auto reformatted with black formatter

--- tests/test_action.py
+++ tests/test_action.py
@@ -1,20 +1,35 @@
 import pytest
 from mockito import when, mock, unstub
 import botocore.vendored.requests.api as requests
+from chalicelib.action import Action
 
-@pytest.fixture
-def action():
-    fake_payload = dict(
-        text=['fake action', 'test'],
-        response_url=['http://fakeurl.nowhere'],
-        user_id=['fake user']
-    )
-    fake_config = dict(slack_token='fake token')
-    from chalicelib.action import Action
-    return Action(fake_payload, fake_config)
 
-def test_perform_unsupported_action(action):
+fake_payload = dict(
+    text=["fake action", "test"],
+    response_url=["http://fakeurl.nowhere"],
+    user_id=["fake user"],
+)
+fake_config = dict(slack_token="fake token")
+
+
+def test_perform_unsupported_action():
+    action = Action(fake_payload, fake_config)
     when(requests).post(
-        url=action.response_url, json={'text': 'Unsupported action: fake'}, headers={'Content-Type': 'application/json'}
-    ).thenReturn(mock({'status_code': 200}))
-    assert action.perform_action() == ''
+        url=action.response_url,
+        json={"text": "Unsupported action: fake"},
+        headers={"Content-Type": "application/json"},
+    ).thenReturn(mock({"status_code": 200}))
+    assert action.perform_action() == ""
+    unstub()
+
+
+def test_perform_edit_action():
+    fake_payload["text"] = ["edit", "fake argument"]
+    action = Action(fake_payload, fake_config)
+    when(requests).post(
+        url=action.response_url,
+        json={"text": "Edit not implemented yet"},
+        headers={"Content-Type": "application/json"},
+    ).thenReturn(mock({"status_code": 200}))
+    assert action.perform_action() == ""
+    unstub()
