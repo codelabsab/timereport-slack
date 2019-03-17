53254056255b03f9292a3518e94b58757647e25e
parberge <paer.berge@gmail.com>
2019-03-17 21:03:33 +0100

    Add method send_response to Action class
    To simplify testing the class and to reduce some duplication of code I
    created a new method called send_response().
    
    In doing this I also refactored the test for add action, and also added
    test for delete action.

--- tests/test_action.py
+++ tests/test_action.py
@@ -4,6 +4,7 @@ import botocore.vendored.requests.api as requests
 from chalicelib.action import Action
 from . import test_data
 
+
 fake_payload = dict(
     text=["unsupported args"],
     response_url=["http://fakeurl.nowhere"],
@@ -39,13 +40,15 @@ def test_perform_add_action():
     fake_payload["text"] = ["add vab 2019-01-01"]
     fake_payload["user_name"] = "fake username"
     action = Action(fake_payload, fake_config)
-    when(requests).post(
-        url="https://slack.com/api/chat.postMessage",
-        json=test_data.add_response,
-        headers={
-            "Content-Type": "application/json; charset=utf-8",
-            "Authorization": "Bearer fake token",
-        },
-    ).thenReturn(mock({"status_code": 200}))
+    when(action).send_response().thenReturn()
+    assert action.perform_action() == ""
+    unstub()
+
+
+def test_perform_delete_action():
+    fake_payload["text"] = ["delete 2019-01-01"]
+    fake_payload["user_name"] = "fake_username"
+    action = Action(fake_payload, fake_config)
+    when(action).send_response().thenReturn()
     assert action.perform_action() == ""
     unstub()
