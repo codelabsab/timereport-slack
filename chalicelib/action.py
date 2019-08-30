9386e688c2d8059da965fefc7ce2925858b8a6b0
parberge <paer.berge@gmail.com>
2019-08-30 16:10:38 +0200

    Add logging in _lock_action

--- chalicelib/action.py
+++ chalicelib/action.py
@@ -190,7 +190,9 @@ class Action:
         /timereport-dev lock 2019-08
         """
         event = create_lock(user_id=self.user_id, event_date=self.params[1])
+        log.debug(f"lock event: {event}")
         response = lock_event(url=self.config['backend_url'], event=event)
+        log.debug(f"response was: {response.text}")
         if response.status_code == 200:
             self.send_response(message=f"Lock successful! :lock: :+1:")
             return ""
