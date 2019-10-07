eb0cb56bf993f3ca1304f503ee1090d64bdcb1d4
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-07 22:17:43 +0200

    Re-structure code for readability

--- chalicelib/lib/interactive.py
+++ chalicelib/lib/interactive.py
@@ -1,9 +1,63 @@
-from chalicelib.lib.slack import slack_payload_extractor, slack_responder, verify_token
+import os
+
+from chalice import Chalice
+
+from chalicelib.lib.slack import slack_payload_extractor, slack_responder
+from chalicelib.lib.security import verify_token
 from chalicelib.lib import api
 from datetime import datetime, timedelta
 
 
+def interactive_handler(app: Chalice):
+    """
+    Extracts data from request and checks if
+    users wants to submit or not.
+
+    Passes payload to correct function that
+    executes logic preparing data to be sent
+    to backend api
+    """
+
+    # store headers and request
+    headers = app.current_request.headers
+    request = app.current_request.raw_body.decode()
+
+    # verify validity of request
+    secret = os.getenv('signing_secret')
+    if not verify_token(headers, request, secret):
+        return 'Slack signing secret not valid'
+
+    # extract slack payload from request and store some vars
+    payload = slack_payload_extractor(request)
+    submit = payload.get('actions')[0].get('value')
+    action = payload.get('callback_id')
+
+    # did user press yes?
+    if submit == "submit_yes":
+        # did user want to add?
+        if action == 'add':
+            # send fields containing order to add function
+            # for further processing and send to backend api
+            interactive_add(
+                payload['original_message']['attachments'][0]['fields']
+            )
+            # TODO:
+            #      slack_responder here?
+        # or did user want to delete?
+        if action == 'delete':
+            interactive_delete(payload)
+            # TODO:
+            #      slack_responder here?
+    # user press no
+    else:
+        slack_responder(url=payload.get("response_url"), msg="Action canceled")
+        return ''
+
+
 def date_range(start_date, stop_date):
+    # TODO:
+    #      Should be removed when we get correct data
+    #      from command
     delta = timedelta(days=1)
     while start_date <= stop_date:
         yield start_date
@@ -22,8 +76,8 @@ def interactive_delete(payload):
     date = payload['original_message']['attachments'][0]['fields'][0].get('value')
     api.delete(user_id, date)
     # TODO:
-    #  should slack_respond here if successful?
-    #  or let interactive_handler manage responses
+    #      should slack_respond here if successful?
+    #      or let interactive_handler manage responses
 
 
 def interactive_add(payload):
@@ -41,7 +95,7 @@ def interactive_add(payload):
 
     Sends events to backend api for persistent storage
 
-    :param payload: containing:
+    payload:
     [
                     {
                         'title': 'User',
@@ -78,48 +132,8 @@ def interactive_add(payload):
         }
         api.create(event)
     # TODO:
-    #  should slack_respond here if successful?
-    #  or let interactive_handler manage responses
+    #      should slack_respond here if successful?
+    #      or let interactive_handler manage responses
 
 
-def interactive_handler(app):
-    """
-    Extracts data from request and checks if
-    users wants to submit or not.
 
-    Passes payload to correct function that
-    executes logic preparing data to be sent
-    to backend api
-    """
-
-    # store headers and request
-    headers = app.current_request.headers
-    request = app.current_request.raw_body.decode()
-
-    # verify validity of request
-    if not verify_token(headers, request, config['signing_secret']):
-        return 'Slack signing secret not valid'
-
-    # extract slack payload from request
-    payload = slack_payload_extractor(request)
-
-    # did user press yes?
-    if payload.get('actions')[0].get('value') == "submit_yes":
-        # did user want to add?
-        if payload.get('callback_id') == 'add':
-            # send fields containing order to add function
-            # for further processing and send to backend api
-            interactive_add(
-                payload['original_message']['attachments'][0]['fields']
-            )
-            # TODO:
-            #  slack_responder here?
-        # or did user want to delete?
-        if payload.get('callback_id') == 'delete':
-            interactive_delete(payload)
-            # TODO:
-            #  slack_responder here?
-    # user press no
-    else:
-        slack_responder(url=payload.get("response_url"), msg="Action canceled")
-        return ''
