b025b2df5d66842fb91183fdccea04b9fa232d86
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-09 23:30:42 +0200

    Remove dead code

--- chalicelib/model/event.py
+++ chalicelib/model/event.py
@@ -1,7 +1,7 @@
-import json
-from datetime import datetime
 import logging
+import json
 log = logging.getLogger(__name__)
+
 
 def create_event(user_id, user_name, reason, event_date, hours):
     format_str = "%Y-%m-%d"
@@ -12,32 +12,4 @@ def create_event(user_id, user_name, reason, event_date, hours):
         'event_date': event_date.strftime(format_str),
         'hours': hours,
     }
-    return event
-
-def create_lock(user_id, event_date):
-    format_str = "%Y-%m"
-    try:
-        datetime.strptime(event_date, format_str)
-    except ValueError:
-        log.error(f"The event_date {event_date} isn't a valid format")
-        return False # Returnera tillbaka så action klassen kan rapportera felet till slack?
-
-    event = {
-        'user_id': user_id,
-        'event_date': event_date,
-    }
-    return event
-
-class Event:
-    # Work in progress, not used.
-
-    def __init__(self, user_id, user_name, reason, event_date, hours, fmt):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.reason = reason
-        self.event_date = event_date
-        self.hours = hours
-        self.fmt = fmt
-
-    def to_json(self):
-        return json.dumps(vars(self))
+    return json.dumps(event)
