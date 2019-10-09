c9ba501ef8b53b6b2b90c9a2ce49b99c6846bb9c
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-09 22:43:06 +0200

    Implement backend functionality
    - create
    - delete
    - lock

--- chalicelib/lib/api.py
+++ chalicelib/lib/api.py
@@ -1,6 +1,5 @@
 import botocore.vendored.requests.api as requests
 import logging
-import os
 
 log = logging.getLogger(__name__)
 
@@ -11,81 +10,57 @@ log = logging.getLogger(__name__)
 ####################################################
 
 
-url = os.getenv('backend_url', 'localhost:8000')
+def create(url: str, event: dict):
+    """Create event
 
-
-def create(data):
-    """
-    Create event
-
-    url: URL to backend API
-    data: A dict with the event to add
-
-    :return: requests response object
+    :param url: str: URL to backend API v2
+    :param event: dict: event
+    :return: requests.models.Response
     """
     headers = {'Content-Type': 'application/json'}
-    res = requests.post(url=url, json=data, headers=headers)
+    res = requests.post(url=url, json=event, headers=headers)
     return res
 
 
-def delete(user_id, date):
-    """
-    Delete event for user
+def delete(url: str, user_id: str, date: str):
+    """Delete event for user
 
-    URL: URL to backend API
-    date: Date to delete as a string (2019-01-01)
-    :return:
+    :param url: URL to backend API v2
+    :param date: Date to delete as a string (2019-01-01)
+    :return: requests.models.Response
     """
     # TODO:
     #  Implement security: api should demand token in header
     #  headers = {f"token: {secret_token}"}
-    res = requests.delete(url=f"{url}/users/{user_id}/events/{date}")
-    return res
+    response = requests.delete(url=f"{url}/users/{user_id}/events/{date}")
+    return response
 
 
-def read(url, user_id, date_str):
+def read(url: str, user_id: str, date: str):
+    """Get existing timereport for a user
+
+    :param url: str: URL to backend API v2
+    :param user_id: str: a user id
+    :param date: str: Valid formats: "2019-01", "2019-01-01", "2019-01-02:2019-01-03"
+    :return: requests.models.Response
     """
-    Get existing timereport for a user
+    url = f"{url}/users/{user_id}/events/{date}"
+    headers = {'Content-Type': 'application/json'}
+    response = requests.get(url=url, headers=headers)
+    return response
 
-    :url: The URL to the backend API
-    :user_id: The users user ID
-    :date_str: A string contaning date. Valid formats: "2019-01", "2019-01-01", "2019-01-02:2019-01-03"
+
+def lock(url: str, user_id: str, date: str):
+    """Lock month for user
+
+    :param url: str: URL to backend API v2
+    :param user_id: str: user_id
+    :param date: str: date in "YYYY-mm"
+    :type sent: dict: {"user_id":"foo01","event_date":"2019-02"}
+    :return: requests.models.Response
     """
-    api_url = f"{url}/event/users/{user_id}"
-    try:
-        start_date, end_date = date_str.split(":")
-    except ValueError as error:
-        log.debug(f"Failed to split: {date_str}")
-
-        if len(date_str.split("-")) == 2:
-            start_date = f"{date_str}-01"
-            end_date = f"{date_str}-31"
-        else:
-            start_date, end_date = date_str, date_str
-
-    except Exception as error:
-        log.debug(f"Unexpected exception. Error was: {error}", exc_info=True)
-        return False
-
-    date_str = {"startDate": start_date, "endDate": end_date}
-
-    response = requests.get(url=api_url, params=date_str)
-    if response.status_code == 200:
-        return response.text
-    else:
-        log.debug(f"Got response code {response.status_code} for user ID {user_id}")
-        return False
-
-
-def lock(url, data):
-    """
-    Lock month for user
-
-    URL: URL to backend API
-    event: The lock event document
-    :return:
-    """
-    headers={'Content-Type': 'application/json'}
     url = f'{url}/locks'
+    headers = {'Content-Type': 'application/json'}
+    data = {"user_id": user_id, "event_date": date}
     response = requests.post(url=url, data=data, headers=headers)
     return response
