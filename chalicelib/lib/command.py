966418c0f1828f81f96f0de5e55bfa4086bffe1e
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-09 23:48:03 +0200

    Add validate functions and start implementing add()

--- chalicelib/lib/command.py
+++ chalicelib/lib/command.py
@@ -9,11 +9,11 @@ from dateutil.parser import parse
 
 
 def command_handler(app: Chalice):
-    """
-    Takes chalice app, extracts headers and request
+    """Takes chalice app, extracts headers and request
     validates token and sends extracted data
     to the correct function handler
     """
+
     # store headers and request
     headers = app.current_request.headers
     request = app.current_request.raw_body.decode()
@@ -54,13 +54,52 @@ def command_handler(app: Chalice):
 
 
 def add(payload: dict):
+    """Checks if reason and date are valid strings
+    If date is range ("2019-12-28:2020-01-03") it will
+    test if range is valid
+    :param payload: dict
+        :type command:
+            :param action: str "add"
+            :param reason: str ["vab", "sick", "intern"]
+            :param date: str ["2019-12-28", "today", "today 8", "today 24", "2019-12-28:2020-01-03"]
+            :param hours: str: optional - hours is optional and defaults to 8 if not given
+
+    Example data of command:
+        command = "add vacation 2019-12-28:2020-01-03"
+        command = "add sick today"
+        command = "add vab 2019-12-28 4"
     """
-    :return:
-    """
-    return NotImplemented
+
+    # extract text from slash command
+    commands = payload['text'][0].split()
+
+    # extract date or date range
+
+    if not validate_reason(commands[1]):
+        return "Not a valid reason"
+
+    # validate hours
+    try:
+        if not validate_hours(commands[3]):
+            return "Not valid hours"
+    except IndexError:
+        hours = 8
+
+    # TODO: implement correct logic
+    #      [] validate reason
+    #      [] validate date and range
+    #      [] validate hours
+
+    r = api.create(
+        url=os.getenv('backend_url'),
+        event=event
+    )
+    if r.status_code != 200:
+        return "fail!"
 
 
 def delete(payload: dict):
+    """Extracts user_id and date from payload and calls api.delete()"""
     date: str = payload['text'][0].split()[-1]
     if parse(date, fuzzy=False):
         r = api.delete(
@@ -80,6 +119,7 @@ def ls(payload: dict):
 
 
 def lock(payload: dict):
+    """Extracts information from payload and calls api.lock()"""
     # store date
     date: str = payload['text'][0].split()[-1]
     # check if date is valid
@@ -111,3 +151,31 @@ def help_menu(url: str):
         """
     slack_responder(url=url, msg=msg)
     return ""
+
+
+def validate_reason(reason: str) -> bool:
+    # TODO: should probably move to other lib
+    """Validates a given reason for an event
+    :param reason: str
+    :return: bool
+    """
+    list_of_reasons: (
+        "vab",
+        "sick",
+        "intern",
+        "vacation",
+    )
+    if reason in list_of_reasons:
+        return True
+    else:
+        return False
+
+
+def validate_hours(hours: str) -> bool:
+    # TODO: should probably move to other lib
+    if round(float(hours)) > 8:
+        return False
+    elif round(float(hours)) < 0:
+        return False
+    else:
+        return True
