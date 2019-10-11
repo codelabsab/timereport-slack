100392c8b29d61ff54c01eecfdbff4325d15b180
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-11 08:14:07 +0200

    Remove usage of ruamel-yaml and loading config file from disk

--- chalicelib/lib/helpers.py
+++ chalicelib/lib/helpers.py
@@ -1,20 +1,3 @@
-from ruamel.yaml import YAML
-
-yaml=YAML(typ='safe')
-
-
-def parse_config(path='config.yaml'):
-    """
-    Parse config written in yaml
-    :param path: the path to the config file. config.yaml is default
-    :return: config
-    """
-    with open(path) as fd:
-        config = yaml.load(fd)
-
-    return config
-
-
 def verify_reasons(valid_reasons, reason):
     if reason in valid_reasons:
         return True
