943f7c9440c64da21e1353b2c9b25795aac689bc
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-11 08:11:05 +0200

    Use global str instead of config dict

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from chalice import Chalice
 import os
 import logging
-from chalicelib.lib.helpers import parse_config
 from chalicelib.lib.interactive import interactive_handler
 from chalicelib.lib.command import command_handler
 
 
+# Set globals or die
+TOKEN: str = f"{os.getenv('bot_access_token')}"
+SECRET: str = f"{os.getenv('signing_secret')}"
+API_URL: str = f"{os.getenv('backend_url')}"
+
+# TODO: REASONS should load from env vars too
+REASONS: tuple = ("vab", "sick", "intern", "vacation")
+
+# Configure logger or die
+logger = logging.getLogger()
+logger.setLevel(os.getenv('log_level'))
+
+
 app = Chalice(app_name='timereport')
 app.debug = True
-
-
-logger = logging.getLogger()
-
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-config = parse_config(f'{dir_path}/chalicelib/config.yaml')
-config['backend_url'] = os.getenv('backend_url')
-config['bot_access_token'] = os.getenv('bot_access_token')
-logger.setLevel(config['log_level'])
 
 
 @app.route('/interactive', method='POST', content_types=['application/x-www-form-urlencoded'])
@@ -26,5 +28,5 @@ def interactive():
 
 
 @app.route('/command', methods=['POST'], content_types=['application/x-www-form-urlencoded'])
-def index():
+def command():
     return command_handler(app)
