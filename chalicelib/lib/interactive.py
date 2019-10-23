c88dcb9c08a0a0252d3234e404c60c1d72dad623
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-23 23:04:49 +0200

    updates to interactivelib

--- chalicelib/lib/interactive.py
+++ chalicelib/lib/interactive.py
@@ -1,10 +1,12 @@
 import os
+import requests
 
 from chalice import Chalice
 
 from chalicelib.lib.slack import slack_payload_extractor, slack_responder, verify_token
 from chalicelib.lib import api
 from datetime import datetime, timedelta
+from app import config
 
 
 def interactive_handler(app: Chalice):
@@ -22,14 +24,15 @@ def interactive_handler(app: Chalice):
     request = app.current_request.raw_body.decode()
 
     # verify validity of request
-    secret = os.getenv('signing_secret')
+    secret: str = os.getenv('signing_secret')
     if not verify_token(headers, request, secret):
         return 'Slack signing secret not valid'
 
     # extract slack payload from request and store some vars
-    payload = slack_payload_extractor(request)
-    submit = payload.get('actions')[0].get('value')
-    action = payload.get('callback_id')
+    payload: dict = slack_payload_extractor(request)
+    submit: str = payload.get('actions')[0].get('value')
+    action: str = payload.get('callback_id')
+    user_id: str = payload.get('user_id').get('id')
 
     # did user press yes?
     if submit == "submit_yes":
@@ -37,16 +40,25 @@ def interactive_handler(app: Chalice):
         if action == 'add':
             # send fields containing order to add function
             # for further processing and send to backend api
-            interactive_add(
-                payload['original_message']['attachments'][0]['fields']
-            )
-            # TODO:
-            #      slack_responder here?
+            url: str = config["backend_url"]
+            fields: dict = payload['original_message']['attachments'][0]['fields']
+            results: list = interactive_add(url=url, user_id=user_id, fields=fields)
+            for result in results:
+                if result.status_code != 200:
+                    # slack_bot_responder here failed
+                    return ""
+                else:
+                    # slack_bot_responder here success
+                    return ""
         # or did user want to delete?
         if action == 'delete':
-            interactive_delete(payload)
-            # TODO:
-            #      slack_responder here?
+            result = interactive_delete(url=url, payload=payload)
+            if result.status_code != 200:
+                # slack_bot_responder here failed
+                return ""
+            else:
+                # slack_bot_responder here success
+                return ""
     # user press no
     else:
         slack_responder(url=payload.get("response_url"), msg="Action canceled")
@@ -63,28 +75,27 @@ def date_range(start_date, stop_date):
         start_date += delta
 
 
-def interactive_delete(payload):
+def interactive_delete(url, payload) -> requests.models.Response:
     """
     Takes a payload and extracts user_id and date
     Calls backend api to delete all events for
     given user and date
     """
     user_id = payload['user'].get('id'),
-    date = payload['original_message']['attachments'][0]['fields'][0].get('value')
-    api.delete(user_id, date)
-    # TODO:
-    #      should slack_respond here if successful?
-    #      or let interactive_handler manage responses
+    date: str = payload['original_message']['attachments'][0]['fields'][0].get('value')
+
+    return api.delete_event(url=url, user_id=user_id, date=date)
 
 
-def interactive_add(payload):
+def interactive_add(url: str, user_id: str, fields: dict) -> list:
     """
     This function takes a payload described in the form
     described below.
 
     Creates events in the form of:
     event = {
-            'user_name': "",
+            'user_id': user_id,
+            'user_name': f"{payload[0].get('value')}"
             'reason': f"{payload[1].get('value')}",
             'event_date': f"{date.strftime('%Y-%m-%d')}",
             'hours': f"{payload[4].get('value')}"
@@ -117,19 +128,23 @@ def interactive_add(payload):
                     }
                 ]
     """
-    start = datetime.strptime(payload[2].get('value'), "%Y-%m-%d")
-    stop = datetime.strptime(payload[3].get('value'), "%Y-%m-%d")
+
+    res = []
+
+    # create date range
+    start = datetime.strptime(fields[2].get('value'), "%Y-%m-%d")
+    stop = datetime.strptime(fields[3].get('value'), "%Y-%m-%d")
+
     for date in date_range(start, stop):
         event = {
-            'user_name': f"{payload[0].get('value')}",
-            'reason': f"{payload[1].get('value')}",
+            "user_id": user_id,
+            'user_name': f"{fields[0].get('value')}",
+            'reason': f"{fields[1].get('value')}",
             'event_date': f"{date.strftime('%Y-%m-%d')}",
-            'hours': f"{payload[4].get('value')}"
+            'hours': f"{fields[4].get('value')}"
         }
-        api.create(event)
-    # TODO:
-    #      should slack_respond here if successful?
-    #      or let interactive_handler manage responses
+        res.append(api.create_event(url, event))
 
+    return res
 
 
