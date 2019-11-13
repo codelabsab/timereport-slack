25f18c394ff1a45ce1785383709730a5640faf87
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-11-13 16:55:24 +0100

    Implement lock check for delete action

--- chalicelib/lib/api.py
+++ chalicelib/lib/api.py
@@ -1,8 +1,4 @@
 import requests
-import logging
-
-
-log = logging.getLogger(__name__)
 
 #####################################################
 #        implementation of timereport-api v2        #
@@ -18,7 +14,6 @@ def create_event(url: str, event: dict) -> requests.models.Response:
     """
     url = f"{url}/events"
     headers = {"Content-Type": "application/json"}
-    log.debug(f"url: {url} - headers: {headers}")
     response = requests.post(url=url, json=event, headers=headers)
     return response
 
@@ -33,7 +28,6 @@ def read_event(url: str, user_id: str, date: str) -> requests.models.Response:
     """
     url = f"{url}/users/{user_id}/events/{date}"
     headers = {"Content-Type": "application/json"}
-    log.debug(f"url: {url} - user_id: {user_id} - headers: {headers}")
     response = requests.get(url=url, headers=headers)
     return response
 
@@ -47,7 +41,6 @@ def delete_event(url: str, user_id: str, date: str) -> requests.models.Response:
     :return: requests.models.Response
     """
     url = f"{url}/users/{user_id}/events/{date}"
-    log.debug(f"url: {url} - user_id: {user_id} - date: {date}")
     response = requests.delete(url=url)
     return response
 
@@ -64,21 +57,20 @@ def create_lock(url: str, user_id: str, date: str) -> requests.models.Response:
     url = f"{url}/locks"
     headers = {"Content-Type": "application/json"}
     data = {"user_id": user_id, "event_date": date}
-    log.debug(f"url: {url} - data: {data} - headers: {headers}")
     response = requests.post(url=url, data=data, headers=headers)
     return response
 
 
-def read_lock(url: str, user_id: str) -> requests.models.Response:
+def read_lock(url: str, user_id: str, date: str) -> requests.models.Response:
     """
     List locks for user. Response contains a list of all locks for user
     and will need to get parsed on our side.
     :param url: str
     :param user_id: str
+    :param date: str
     :return: requests.models.Response
     """
-    url = f"{url}/users/{user_id}/locks"
+    url = f"{url}/lock/users/{user_id}/{date}"
     headers = {"Content-Type": "application/json"}
-    log.debug(f"url: {url} - user_id: {user_id} - headers: {headers}")
     response = requests.get(url=url, headers=headers)
     return response
