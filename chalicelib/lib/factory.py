42e3d8ee5a744cd3fb0de480b82193899cf2ccee
parberge <paer.berge@gmail.com>
2019-11-13 15:13:36 +0100

    Add missing import in factory.py
    And remove old factory function

--- chalicelib/lib/factory.py
+++ chalicelib/lib/factory.py
@@ -1,57 +1,9 @@
 from datetime import datetime, timedelta
 from ..model.event import create_event
+from chalicelib.lib.helpers import date_range
 import logging
 
 log = logging.getLogger(__name__)
-
-
-# def factory(order):
-#     """
-#     Create correct format from order
-#     :param order: The order object
-#     :return: list
-#     """
-#     format_str = "%Y-%m-%d"
-#     dates, events = [], []
-#     user_id, user_name = order['user_id'], order['user_name']
-#
-#     cmd = order['text'][0].split()
-#
-#     if len(cmd) < 3 or len(cmd) > 4:
-#         log.error(f'Wrong amount of arguments: {len(cmd)}')
-#         return False
-#
-#     reason, date_str = cmd[1:3]
-#     log.debug(f"Date string is: {date_str}")
-#
-#     try:
-#         hours = round(float(cmd[3]), 1)
-#         if 0 < hours > 8:
-#             hours = 8
-#     except IndexError:
-#         hours = 8
-#
-#     except ValueError:
-#         return False
-#
-#     if ":" in date_str:
-#         date_str_start, date_str_stop = date_str.split(":")
-#         date_obj_start = datetime.strptime(date_str_start, format_str)
-#         date_obj_stop = datetime.strptime(date_str_stop, format_str)
-#         for d in date_range(date_obj_start, date_obj_stop):
-#             dates.append(d)
-#     elif "today" in date_str:
-#         date_obj = datetime.now().date()
-#         dates.append(date_obj)
-#     else:
-#         log.debug(f"Will try to convert string {date_str} to a datetime object")
-#         date_obj = datetime.strptime(date_str, format_str)
-#         dates.append(date_obj)
-#
-#     for event_date in dates:
-#         e = create_event(user_id, user_name, reason, event_date, hours)
-#         events.append(e)
-#     return events
 
 
 def factory(json_order):
