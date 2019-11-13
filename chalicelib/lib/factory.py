8237eb10438cd071f07250c50bd447915007cd96
parberge <paer.berge@gmail.com>
2019-11-13 15:32:25 +0100

    Refactor factory
    We only provide one date object.
    Factory now need logic to split if multiple dates.

--- chalicelib/lib/factory.py
+++ chalicelib/lib/factory.py
@@ -6,14 +6,13 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def factory(json_order):
+def factory(json_order, format_str):
     """
     Extract necessary values from the interactive message sent via slack
 
     :param json_order: A dict based on slack interactive message payload
     :return: list
     """
-    format_str = "%Y-%m-%d"
 
     log.debug(f"json_order is: {json_order}")
 
@@ -21,10 +20,15 @@ def factory(json_order):
 
     user_name = payload[0]["value"]
     reason = payload[1]["value"]
-    start_date = payload[2]["value"]
-    stop_date = payload[3]["value"]
-    hours = payload[4]["value"]
+    date = payload[2]["value"]
+    hours = payload[3]["value"]
     events = []
+
+    start_date = date
+    stop_date = date
+    if ":" in date:
+        start_date, stop_date = date.split(":")
+
     date_obj_start = datetime.strptime(start_date, format_str)
     date_obj_stop = datetime.strptime(stop_date, format_str)
     for date in date_range(date_obj_start, date_obj_stop):
