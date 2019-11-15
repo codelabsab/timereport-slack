0c0ac5af2f5fd37b4e2649879e5cec833d03734a
parberge <paer.berge@gmail.com>
2019-11-15 21:57:45 +0100

    Add parse_date function
    This function will parse the date provided as a string.

--- chalicelib/lib/helpers.py
+++ chalicelib/lib/helpers.py
@@ -1,5 +1,8 @@
 from ruamel.yaml import YAML
 from datetime import timedelta, datetime
+import logging
+
+log = logging.getLogger(__name__)
 
 yaml = YAML(typ="safe")
 
@@ -25,17 +28,43 @@ def date_range(start_date, stop_date):
 
 
 def validate_date(date, format_str) -> bool:
+    """
+    Validate a string in date format is valid
+    """
 
     try:
-        if ":" in date:
-            start, stop = date.split(":")
-            datetime.strptime(start, format_str)
-            datetime.strptime(stop, format_str)
-            if start > stop:
-                return False
-        else:
-            datetime.strptime(date, format_str)
+        datetime.strptime(date, format_str)
     except TypeError:
         return False
 
     return True
+
+
+def parse_date(date: str, format_str: str = "%Y-%m-%d") -> list:
+    """
+    Parse the date from a string.
+    Expects these formats:
+    "today"
+    "2019-01-01"
+    "2019-01-01:2019-01-02"
+    """
+
+    date_list = list()
+    if date == "today":
+        date_list.append(datetime.now())
+        return date_list
+
+    if ":" in date:
+        first_date, second_date = date.split(":")
+
+        if validate_date(first_date, format_str=format_str) and validate_date(second_date, format_str=format_str):
+            date_list.append(datetime.strptime(first_date, format_str))
+            date_list.append(datetime.strptime(second_date, format_str))
+    else:
+        try:
+            if validate_date(date, format_str=format_str):
+                date_list.append(datetime.strptime(date, format_str))
+        except ValueError as error:
+            log.warning(f"Unable to get date from {date}. Error was: {error}")
+
+    return date_list
