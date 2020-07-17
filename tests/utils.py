b41b8716b89c54d2f20150bfc9b120600474bb8c
Mattias Lundberg <mattias.lundberg@codelabs.se>
2020-07-17 07:59:36 +0200

    Document test utils

--- tests/utils.py
+++ tests/utils.py
@@ -11,6 +11,16 @@ from tests.conftest import signing_secret
 def respond_interactively(
     chalice_app, attachments, user_id, user_name, action="submit_yes"
 ):
+    """
+    Mocks slacks menu driven responses, for example used to verify adding entries.
+
+    :param chalice_app: mocked chalice app (from fixture with same name)
+    :param attachments: attachments from call_from_slack, what we're responding to
+    :param user_id: id of the user
+    :param user_name: name of the user
+    :param action: response from user
+    :return: Dict with `slack_message` containing the message to slack and `response` containing the http response
+    """
     payload = dict(
         response_url="example.com",
         user_id=[user_id],
@@ -27,6 +37,15 @@ def respond_interactively(
 def call_from_slack(
     chalice_app, full_command: str, user_id: str, user_name: str,
 ) -> Dict[str, str]:
+    """
+    Mock command sent from slack.
+
+    :param chalice_app: mocked chalice app (from fixture with same name)
+    :param full_command: the command string, as written by a user after `/timereport `
+    :param user_id: id of the user
+    :param user_name: name of the user
+    :return: Dict with `slack_message` containing the message to slack and `response` containing the http response
+    """
     command = full_command.split(" ")[0]
     payload = dict(
         command=command,
@@ -62,5 +81,13 @@ def _perform_call(chalice_app, endpoint, payload):
 
 
 def get_raw_block_text(slack_message):
+    """
+    Parse a slack message returning the raw text from "blocks"
+
+    :param slack_message: Slack message as returned from call_from_slack
+    :return: String with the raw text (without formatting)
+    """
     blocks = slack_message[1]["json"]["blocks"]
+    # Flatten nested dict:
+    # blocks = [{"text": {"text": "Some text"}}]
     return "".join([b.get("text", {}).get("text", "") for b in blocks])
